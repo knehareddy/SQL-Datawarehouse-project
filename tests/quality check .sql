0f6f0929d0dedba3b52ea6dfbb2d5ec688d2f76c
knehareddy <148938536+knehareddy@users.noreply.github.com>
2025-11-01 15:17:20 +0530

    Update quality check .sql

--- tests/quality check .sql
+++ tests/quality check .sql
@@ -44,6 +44,32 @@ where cst_gndr!=trim(cst_gndr);
 select distinct cst_marital_status
 from silver.crm_cust_info;
 
+--Gold Layer
+-- Checking 'gold.dim_customers'
+SELECT 
+    customer_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
+
+-- Checking 'gold.dim_products'
+SELECT 
+    product_key,
+    COUNT(*) AS duplicate_count
+FROM gold.dim_products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
+
+--Checking gold.fact_sales
+SELECT * 
+FROM gold.fact_sales f
+LEFT JOIN gold.dim_customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN gold.dim_products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL OR c.customer_key IS NULL
+
 select distinct cst_gndr
 from silver.crm_cust_info;
 
